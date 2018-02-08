5ff0f20f8168d8e4f205793923f6bbdc8945a1e5
mkef01 <mkef01@gmail.com>
2018-02-07 23:22:22 -0600

    agregado ddl

--- LINQ_Ejemplo/Controllers/InicioController.cs
+++ LINQ_Ejemplo/Controllers/InicioController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using LINQ_Ejemplo.Models;
 
 namespace LINQ_Ejemplo.Controllers
@@ -52,11 +53,75 @@ namespace LINQ_Ejemplo.Controllers
 
             return View(detalleLista);
         }
+
+        public ActionResult Alumno()
+        {
+            var lista = DdlAlumnos();
+            var list = new SelectList(lista, "codigo", "nombre");
+            ViewData["alumnos"] = list;
+            ViewData["autorizar"] = null;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Alumno(string seleccion)
+        {
+            var idi = seleccion;
+            var lista = DdlAlumnos();
+            var list = new SelectList(lista, "codigo", "nombre");
+            ViewData["alumnos"] = list;
+            IList<LibrosAlumno> detalleLista = new List<LibrosAlumno>();
+            var query = from alum in _context.alumnos
+                join pre in _context.prestamos on alum.codalumno equals pre.codalumno
+                join ejem in _context.ejemplares on pre.codejemplar equals ejem.codejemplar
+                join lib in _context.libros on ejem.codlibro equals lib.codlibro
+                where alum.codalumno == seleccion
+                select new
+                {
+                    Nombre = alum.nombre,
+                    Fecha = pre.fechaprestamo,
+                    Libro = lib.titulo
+                };
+            var listado = query.ToList();
+            foreach (var listaEstudiante in listado)
+            {
+                detalleLista.Add(new LibrosAlumno()
+                {
+                   Nombre = listaEstudiante.Nombre,
+                   Fecha = listaEstudiante.Fecha.ToString(),
+                   Titulo = listaEstudiante.Libro
+                    
+                });
+            }
+            ViewData["autorizar"] = "entrar";
+            return View(detalleLista);
+        }
+
+        private List<LibrosAlumno> DdlAlumnos()
+        {
+            var alumnosDdl = new List<LibrosAlumno>();
+            var query = from alum in _context.alumnos
+                select new
+                {
+                    Nombre = alum.nombre,
+                    Codigo = alum.codalumno
+                };
+            var lista = query.ToList();
+            foreach (var ddlLista in lista)
+            {
+                alumnosDdl.Add(new LibrosAlumno()
+                {
+                    Codigo = ddlLista.Codigo,
+                    Nombre = ddlLista.Nombre
+                });
+            }
+            return alumnosDdl;
+        }
 	}
 }
 
 
-//select codlibro, titulo, tema, editorial, idioma, precio, year from libros bs
-//inner join tematicas ts on ts.codtema = bs.codtema
-//inner join editoriales es on es.codeditorial = bs.codeditorial
-//inner join idiomas idi on idi.codidioma = bs.codidioma
+//select alum.nombre, pre.fechaprestamo, lib.titulo from alumnos alum
+//inner join prestamos pre on alum.codalumno = pre.codalumno
+//inner join ejemplares ejem on ejem.codejemplar = pre.codejemplar
+//inner join libros lib on lib.codlibro = ejem.codlibro where alum.nombre = 'Ana Toledo'
---
+++ LINQ_Ejemplo/Models/librosAlumno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LINQ_Ejemplo.Models
+{
+    public class LibrosAlumno
+    {
+        public string Nombre { get; set; }
+        public string Fecha { get; set; }
+        public string Titulo { get; set; }
+        public string Codigo { get; set; }
+    }
+}
